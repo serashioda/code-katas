b0a7bbf0e882373eb9fb420d3c15d2895cb5d325
Your Name <you@example.com>
2016-12-10 18:14:32 -0800

    restructured directory structure

--- source/count_bits.py
+++
@@ -1,7 +0,0 @@
-""" Implemntation of the Bit Counting Kata. """
-def countBits(n):
-    """ Converts a number to binary and counts the number of 1 bits. """
-    print(n)
-    val = bin(n)
-    print(val)
-    return val.count("1")
--- source/multiply.py
+++
@@ -1,6 +0,0 @@
-"""Implementation of the Kata multiply."""
-
-
-def multiply(a, b):
-    """takes two integers and returns its product. """
-    return a * b
