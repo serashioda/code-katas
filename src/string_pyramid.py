579735e64994ed5e764f9eb48595d53a355cef92
Your Name <you@example.com>
2017-02-21 22:24:14 -0800

    def watch_pyramid_from_above done. starting tests.

--- src/string_pyramid.py
+++ src/string_pyramid.py
@@ -1,8 +1,8 @@
-"""Implementation of the string_pyramid."""
+"""Implementation of the string pyramid."""
+
 
 def watch_pyramid_from_the_side(characters):
     """Display side of pyramid."""
-
     characters = characters[::-1]
     c = len(characters)
     rows = []
@@ -16,3 +16,18 @@ def watch_pyramid_from_the_side(characters):
 
 def watch_pyramid_from_above(characters):
     """Display pyramid from above."""
+    c = len(characters)
+    rows = []
+
+    for i in range(c):
+        num_repeat = (c - (i + 1)) * 2 + 1
+        pre_pend = ''
+        post_pend = ''
+        if i > 0:
+            pre_pend = characters[:i]
+            post_pend = characters[0:i][::-1]
+        rows.append(pre_pend + characters[i] * num_repeat + post_pend)
+
+    for row in rows[::-1][1:]:
+        rows.append(row)
+    return '\n'.join(rows)
