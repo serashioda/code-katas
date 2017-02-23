990e34718c08251b4726ec819780464e7d4fb623
Your Name <you@example.com>
2017-02-22 22:10:48 -0800

    updated forbes.py to work with test file. Tests written. All passing.

--- src/forbes.py
+++ src/forbes.py
@@ -1,26 +1,31 @@
-"""."""
-
+"""Implementation of the forbes module."""
 import json
-s = open('forbes.json', 'r').read()
-data = json.loads(s)
-print(data)
 
 
-# returns the name, net worth, and industry of the oldest billionaire under 80 
-# years old AND the youngest billionaire with a valid age.
+def get_json():
+    """Grab json data."""
+    s = open('src/forbes.json', 'r').read()
+    data = json.loads(s)
+    return data
 
-# Write another function that takes the company owned by the oldest under 80 and youngest billionaire and 
-# scrapes the web for its current stock price. 
-# If the company is not public, have an appropriate message. 
-# If the company is not an actual company, have an appropriate message.
+
+"""Write a function that returns the name, net worth, and industry of:
+1) the oldest billionaire under 80 years old, AND
+2) the youngest billionaire with a valid age.
+
+STRETCH: Write another function that takes the company owned by the oldest under 80 and youngest billionaire, and
+1) scrapes the web for its current stock price.
+2) If the company is not public, have an appropriate message.
+3) If the company is not an actual company, have an appropriate message.
+"""
+
 
 def find_billionaire(data):
-    """."""
+    """Find both oldest under 80 and youngest valid billionaires using the two helper methods."""
     result = {
         'oldest_under_80': forbes_find_oldest_under_80(data),
         'youngest_valid': forbes_find_youngest_valid_age(data)
     }
-
     return result
 
 
@@ -31,19 +36,12 @@ def forbes_find_oldest_under_80(data):
         if person['age'] < 80 and curr['age'] < person['age']:
             curr = person
     return curr
-# result = forbes_find_oldest_under_80(data)
-# print(result)
 
 
 def forbes_find_youngest_valid_age(data):
     """Find the youngest billionaire with a valid age."""
     curr = data[0]
     for person in data:
-        if person['age'] > 0 and person['age'] < curr['age']:
+        if person['age'] >= 0 and person['age'] < curr['age']:
             curr = person
     return curr
-# result = forbes_find_youngest_valid_age(data)
-# print(result)
-
-result = find_billionaire(data)
-print(result)
