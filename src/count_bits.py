5a827093d75c5b44236aac4733eb9b5800cfe1b9
Your Name <you@example.com>
2017-02-23 19:44:25 -0800

    .gitignore

--- src/count_bits.py
+++ src/count_bits.py
@@ -1,7 +1,7 @@
-""" Implemntation of the Bit Counting Kata. """
+"""Implemntation of the Bit Counting Kata."""
+
+
 def countBits(n):
-    """ Converts a number to binary and counts the number of 1 bits. """
-    print(n)
+    """Convert a number to binary and counts the number of 1 bits."""
     val = bin(n)
-    print(val)
     return val.count("1")
